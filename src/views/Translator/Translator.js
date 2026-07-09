547e9d484180302c64b78aa48a8890d93f274dfc
Patryk Tomaszek <patryktomaszek6@gmail.com>
2026-07-09 14:30:31 +0200

    Added displaying & sorting data from json file

--- src/views/Translator/Translator.js
+++ src/views/Translator/Translator.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Data from './TestData/TestData.json';
 
-const Translator = () => {
-    return(
-        <div></div>
+const tablica = Object.entries(Data);
+function Translator() {
+    return (
+        <div className="container mt-4">
+            <ul className="list-group">
+                {tablica.sort((a, b) => {
+                    const keyA = a[0];
+                    const keyB = b[0];
+                    return keyA.localeCompare(keyB);
+                }).map(([key, value]) => (
+                    <li key={key} className="list-group-item d-flex justify-content-between align-items-center">
+                        <span>
+                            {key}
+                        </span>
+                        <strong>
+                            {value}
+                        </strong>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-};
+}
+
+export default Translator;
